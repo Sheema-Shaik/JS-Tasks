cd6d83e6e61591c4cad9268030ed0d263e8b14b8
Sheema-Shaik <148420931+Sheema-Shaik@users.noreply.github.com>
2026-06-17 18:18:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var employeename ="Sheema"
+console.log("Employee Name:"+employeename);
+let employeeage = 23
+console.log("Employee Age:"+employeeage);
+const employeecompany ="Stackly"
+console.log("Employee Company:"+employeecompany);
+
+alert("Welcome to Employee Portal.");
+let taskcomplete =confirm("Have you completed today's task?");
+console.log(taskcomplete);
+let entername =prompt("Enter your Name:")
+console.log(entername);
+document.writeln("Welcome "+entername);
+console.warn("Task Submitted Successfully. ");
+console.error("Task Submission Failed");
+employeeage = 23
+console.log("Updated Employee Age:"+employeeage);
